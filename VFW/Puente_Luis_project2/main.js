1672103657e094e9fa2f0f639ea7d83562dd2a95
Luis Puente <kenshiryu7@fullsail.edu>
2011-09-13 17:49:27 -0400

    made corrections to typos to allow local storage. (be more careful next time!)

--- VFW/Puente_Luis_project2/main.js
+++ VFW/Puente_Luis_project2/main.js
@@ -22,7 +22,7 @@ function getItems(){
         var prname      = localStorage.getItem("appprname");
         var timesig     = localStorage.getItem("apptimesig");
         var BPMs        = localStorage.getItem("appBPMs");
-        var favirote    = localStorage.getItem("appfavirote");
+        var favorite    = localStorage.getItem("appfavorite");
         var date        = localStorage.getItem("appdate");
         var Notes       = localStorage.getItem("appNotes");
         
@@ -31,7 +31,7 @@ function getItems(){
             prname,
             timesig,
             BPMs,
-            favirote,
+            favorite,
             date,
             Notes
         ];
@@ -45,7 +45,7 @@ function storeData(id) {
     var prname       = document.getElementById("prname").value;
     var timesig      = document.getElementById("timesig").value;
     var BPMs         = document.getElementById("BPMs").value;
-    var favirote     = document.getElementById("favirote").value;
+    var favorite     = document.getElementById("favorite").value;
     var date         = document.getElementById("date").value;
     var Notes        = document.getElementById("Notes").value;
     
@@ -53,7 +53,7 @@ function storeData(id) {
     localStorage.setItem("appprname", prname);
     localStorage.setItem("apptimesig", timesig);
     localStorage.setItem("appBPMs", BPMs);
-    localStorage.setItem("appfavirote", favirote);
+    localStorage.setItem("appfavorite", favorite);
     localStorage.setItem("appdate", date);
     localStorage.setItem("appNotes", Notes);
 }
