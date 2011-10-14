7ad798a18b925fd28cbeb8d4408186c0d27d85c1
Luis Puente <kenshiryu7@fullsail.edu>
2011-10-13 21:43:55 -0400

    made pop up window work for good app

--- MIU/Puente_Luis_project3/JQMobile Template/js/main1.js
+++ MIU/Puente_Luis_project3/JQMobile Template/js/main1.js
@@ -15,11 +15,16 @@ $(document).ready(function(){
     pform.validate({
         invalidHandler: function(form, validator) {
             errorsLink.click();
-            console.log(validator.submitted);
+            //console.log(validator.submitted);
            var html = '';
            for(var key in validator.submitted){
+                var label = $('label[for^="'+ key + '"]').not('[generated]');
+                var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+                var fieldName = legend.length ? legend.text() : label.text();
+                //console.log(fieldName);
+                html += '<li>'+ fieldName + '</li>';
             };
-            $("#errorsLink ul").html(html);
+            $("#recordErrors ul").html(html);
         },
         submitHandler: function() {
             var data = pform.serializeArray();
