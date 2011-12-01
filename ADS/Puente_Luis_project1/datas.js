5150b03add595138cf4d811e9e83d210db212959
Luis Puente <kenshiryu7@fullsail.edu>
2011-12-01 15:50:03 -0500

    wrapped keys in " "

--- ADS/Puente_Luis_project1/datas.js
+++ ADS/Puente_Luis_project1/datas.js
@@ -3,10 +3,10 @@
 $(function(){
 	    
 	    $.ajax({
-		url: 'xhr/json.php',
-		type: 'GET',
-		dataType:'json',
-		success:function(response) {
+		"url": 'xhr/json.php',
+		"type": 'GET',
+		"dataType":'json',
+		"success":function(response) {
 		    console.log(response);
 		}
 	    });
