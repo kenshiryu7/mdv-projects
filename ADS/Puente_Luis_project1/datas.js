b5fd84ae2b4c22976d88254b2b62aefd905bb3d9
Luis Puente <kenshiryu7@fullsail.edu>
2011-12-01 17:35:13 -0500

    made some small changes. Test

--- ADS/Puente_Luis_project1/datas.js
+++ ADS/Puente_Luis_project1/datas.js
@@ -1,4 +1,5 @@
 //
+//Json data function
 
 $(function(){
 	    
@@ -6,6 +7,36 @@ $(function(){
 		"url": 'xhr/json.json',
 		"type": 'GET',
 		"dataType":'json',
+		"success":function(response) {
+		    console.log(response);
+		}
+	    });
+            
+});
+
+//XML data function
+
+$(function(){
+	    
+	    $.ajax({
+		"url": 'xhr/xml.php',
+		"type": 'GET',
+		"dataType":'xml',
+		"success":function(response) {
+		    console.log(response);
+		}
+	    });
+            
+});
+
+//CSV data function
+
+$(function(){
+	    
+	    $.ajax({
+		"url": 'xhr/csv.php',
+		"type": 'GET',
+		"dataType":'csv',
 		"success":function(response) {
 		    console.log(response);
 		}
