92fa9c214b26652bed0056bc77781e9c74d1b690
Luis Puente <kenshiryu7@fullsail.edu>
2011-12-08 09:23:04 -0500

    Drag and drop/ file copy into attachments worked

---
+++ ADS/ASDcouch/app/_attachments/xhr/csv.php
@@ -0,0 +1,6 @@
+practiceC, prname, timesig, bpms, favortie, date, notes
+Hands, left hand ex, 4/4, 100, off, 12-1-11, this is tough
+Feet, left foot speed, 4/4, 110, off, 12-2-11, must relax
+Feet, double bass, 4/4, 160, on, 12-16-11, great speed exercise
+Hands, around the set, 6/8, 90, on, 12-17-11, good stuff
+Feet, doubles, 4/4, 120, on, 12-10-11, awesome
