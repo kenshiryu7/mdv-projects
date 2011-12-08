f2c6bd17dfbda4f91e063d6f9a280de699001be3
Luis Puente <kenshiryu7@fullsail.edu>
2011-12-08 01:17:43 -0500

    hopefully put app file into eclipse

---
+++ ADS/asdcouch/asdcouch/app/_attachments/Puente_Luis_project1 copy/exdata.js
@@ -0,0 +1,53 @@
+var initalData = [
+    {
+    "practiceC":    "Hands",
+    "prname" :      "left hand exc",
+    "timesig":      "4/4",
+    "BPMs":         "60",
+    "favorite":     "",
+    "date":         "9-27-2011",
+    "Notes":        "I hate working the left hand!"
+    },
+    {
+    "practiceC":    "Feet",
+    "prname" :      "right foot exc",
+    "timesig":      "4/4",
+    "BPMs":         "100",
+    "favorite":     "",
+    "date":         "9-27-2011",
+    "Notes":        "great exc for speed"
+    },
+    {
+    "practiceC":    "Drumset",
+    "prname" :      "around the set",
+    "timesig":      "4/4",
+    "BPMs":         "110",
+    "favorite":     "",
+    "date":         "9-27-2011",
+    "Notes":        "great for fills!"
+    },
+    {
+    "practiceC":    "Hands",
+    "prname" :      "speed exc",
+    "timesig":      "4/4",
+    "BPMs":         "220",
+    "favorite":     "",
+    "date":         "9-27-2011",
+    "Notes":        "good build up"
+    },
+    {
+    "practiceC":    "Feet",
+    "prname" :      "speed exc",
+    "timesig":      "4/4",
+    "BPMs":         "200",
+    "favorite":     "",
+    "date":         "9-27-2011",
+    "Notes":        "hard but productive"
+    },
+    
+    
+    
+    
+    
+    
+    
