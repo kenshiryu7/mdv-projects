19b121b778f947dce52aef3da03e6cebc416c06f
Luis Puente <kenshiryu7@fullsail.edu>
2011-12-15 20:11:14 -0500

    some last tweaks / unfinished project.... Need guidance...

--- ADS/ASDcouch/app/_attachments/js/newjs.js
+++ ADS/ASDcouch/app/_attachments/js/newjs.js
@@ -39,6 +39,121 @@ $('#date').live("pageshow", function(){
 	console.log(date);
 	
 
+//Throwing in old CRUD code....
+
+	function getItems() {
+	  
+	   var getListdiv = $('#new');
+	   
+	   for (var i = 0, len = localStorage.length; i < len; i++) {
+	        var key = localStorage.key(i);
+	        var value = localStorage.getItem(key);
+	        value = value.split(";");
+	        var practiceC   =value[0];
+	        var prname      =value[1];
+	        var timesig     =value[2];
+	        var BPMs        =value[3];
+	        var favorite    =value[4];
+	        var date        =value[5];
+	        var Notes       =value[6];
+	        var newDiv = $("<div>");
+	        for (var k = 0, allLength = value.length; k < allLength; k++) {
+	            var newPara = $("<p/>");
+	            var itemTxt = $('value[k]').text();
+	            newPara.appendTo(itemTxt);
+	            newDiv.appendTo(newPara);
+	            getListdiv.appendTo(newDiv);
+	        }
+	   
+	       //attemp to adding images per category
+	        var image ="";
+	            if (practiceC =="") {image ="";}
+	            if ((practiceC =="Hands") || (practiceC =="Feet") || (practiceC =="Drumset")) {image ="Rudiments.png";}
+	            if ((practiceC =="Feet/Hands") || (practiceC =="Hands/Feet")) {image = "Ostinatos.png";}
+	            if ((practiceC =="Jazz") || (practiceC =="Rock") || (practiceC =="Funk")) {image ="beats.png";}
+	        
+	     
+	         //add images   
+	        var newImg = $("<img/>");
+	        var setSrc = newImg.attr("src","img/" + image);
+	        getListdiv.appendTo(newImg);
+	       
+	      
+	   //add delete single item link
+	   var deleteLink = $("<a/>");
+	   var setHref    = deleteLink.attr("href", "#");
+	   var setOnclick = deleteLink.attr("onclick", "deleteItem(" + key + ");");
+	   var deleteText = $("delete item").text();
+	   deleteLink.appendTo(deleteText);
+	   newDiv.appendTo(deleteLink);
+	   
+	   //add edit single item
+	   var editLink         = $("<a/>");
+	   var setEditHref      = editLink.attr("href", "#");
+	   var setEditOnclick   = editLink.attr("onclick", "editItem(" + key + ");");
+	   var editText         = $("edit item").text();
+	   editLink.appendTo(editText);
+	   newDiv.appendTo(editLink);
+	   }
+	 
+	    if(localStorage.length >=1) {
+	        var clearLink = $('#clear').show();
+	        
+	        
+	   
+	    } else {
+	            var  prname    = "enter practice name";
+	            var  timesig   = "enter a time signature";
+	            var  BPMs      = "enter a BPM's"
+	            
+	           $('#prname').val(prname);
+	           $('#timesig').val(timesig); 
+	           $('#BPMs').val(BPMs);
+	        }
+	}	
+	
+//save items function. get date and time from comp...
+	function saveItems(id) {
+	    var d = new Date();
+	    var key = (d.getTime());
+	    var practiceC = $('#practiceC').val();
+	    var prname    = $('#prname').val();
+	    var timesig   = $('#timesig').val();
+	    var BPMs      = $('#BPMs').val();
+	    var favorite  = $('#favorite').val();
+	    var date      = $('#date').val();
+	    var Notes     = $('#Notes').val();
+	    
+	    var allItems  = [
+	            practiceC,
+	            prname,
+	            timesig,
+	            BPMs,
+	            favorite,
+	            date,
+	            Notes
+	        ];
+	    localStorage.setItem(key, allItems.join(";"));
+	}
+
+
+	function storeData(id) {
+	    var practiceC    = $('#practiceC').val();
+	    var prname       = $('#prname').val();
+	    var timesig      = $('#timesig').val();
+	    var BPMs         = $('#BPMs').val();
+	    var favorite     = $('#favorite').val();
+	    var date         = $('#date').val();
+	    var Notes        = $('#Notes').val();
+	    
+	    localStorage.setItem("apppracticeC", practiceC);
+	    localStorage.setItem("appprname", prname);
+	    localStorage.setItem("apptimesig", timesig);
+	    localStorage.setItem("appBPMs", BPMs);
+	    localStorage.setItem("appfavorite", favorite);
+	    localStorage.setItem("appdate", date);
+	    localStorage.setItem("appNotes", Notes);
+	}
 	
 function editItem(id) {
 	    
@@ -122,3 +237,5 @@ function editItem(id) {
     }
 });
 });
+
+$('#log').bind("click",);
