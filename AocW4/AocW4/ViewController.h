7e5c4fecbcde31311ee0bfb3a88158b3e9bf5ddf
Luis Puente <kenshiryu7@fullsail.edu>
2012-04-22 19:16:24 -0400

    begin project 4

---
+++ AocW4/AocW4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AocW4
+//
+//  Created by Luis Puente on 4/22/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
