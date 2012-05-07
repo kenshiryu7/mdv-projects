5135be0f81cd8c72dc285c8b5233a8927039727d
Luis Puente <kenshiryu7@fullsail.edu>
2012-05-07 00:13:35 -0400

    Creating second view based on video. Not sure how to name these properly for functionality yet. Will find out as I go and may rename accordingly.

---
+++ AOC_2/Aoc2_W2/Aoc2_W2/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Aoc2_W2
+//
+//  Created by Luis Puente on 5/7/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
