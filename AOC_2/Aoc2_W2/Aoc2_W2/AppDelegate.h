dde92b4f942fb7e32a5fa77c8af896d27d99823a
Luis Puente <kenshiryu7@fullsail.edu>
2012-05-06 23:30:44 -0400

    Begin New Project single view for Week 2

---
+++ AOC_2/Aoc2_W2/Aoc2_W2/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Aoc2_W2
+//
+//  Created by Luis Puente on 5/6/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AOC_2/Aoc2_W2/Aoc2_W2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Aoc2_W2
+//
+//  Created by Luis Puente on 5/6/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
