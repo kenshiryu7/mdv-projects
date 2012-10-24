7e29e4d02ffd1dd1d5146fa29154a6ddc2b6d0d1
Luis Puente <kenshiryu7@fullsail.edu>
2012-10-24 16:19:41 -0400

    Followed the Text View Video and Virtual Device. Made some changes for testing and setting up.

--- Java_1/TestingAgain/src/com/lpuente/testingagain/MainActivity.java
+++ Java_1/TestingAgain/src/com/lpuente/testingagain/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -15,10 +16,21 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        //Creating a layout, instantiating and applying width and height params.
         ll = new LinearLayout(this);
         ll.setOrientation(LinearLayout.VERTICAL);
         lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         ll.setLayoutParams(lp);
+        
+        //creating text view / instantiating
+        TextView tv = new TextView(this);
+        tv.setText("Just an example text view.");
+        
+        //adding text view to the layout
+        ll.addView(tv);
+        
+        //adding layout to the content view
+        setContentView(ll);
     }
 
     @Override
