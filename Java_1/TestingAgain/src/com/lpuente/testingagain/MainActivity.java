d0d8c5eb4facf6a1ec070375e90486fd57d17632
Luis Puente <kenshiryu7@fullsail.edu>
2012-10-24 20:07:54 -0400

    Followed Edit / button video. Minor changes

--- Java_1/TestingAgain/src/com/lpuente/testingagain/MainActivity.java
+++ Java_1/TestingAgain/src/com/lpuente/testingagain/MainActivity.java
@@ -3,6 +3,8 @@ package com.lpuente.testingagain;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,20 +18,30 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        //Creating a layout, instantiating and applying width and height params.
+      //Creating a layout, instantiating and applying width and height params.
         ll = new LinearLayout(this);
         ll.setOrientation(LinearLayout.VERTICAL);
         lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         ll.setLayoutParams(lp);
         
-        //creating text view / instantiating
+      //creating text view / instantiating
         TextView tv = new TextView(this);
         tv.setText("Just an example text view.");
         
-        //adding text view to the layout
+      //adding text view to the layout
         ll.addView(tv);
         
-        //adding layout to the content view
+      //Edit text class/instantiate  
+        EditText et = new EditText(this);
+        et.setHint("User type here");	//adds text within text line /dissapears as user types
+        ll.addView(et);
+      
+      //edit text button
+        Button  ebutt = new Button(this);
+        ebutt.setHint("Button 1");		//set text within button. Instruction
+        ll.addView(ebutt); //adding button to the Layout
+        
+      //adding layout to the content view
         setContentView(ll);
     }
 
