199a6f8476b3ff62f50f8e17e32cc4f805ea8d61
Luis Puente <kenshiryu7@fullsail.edu>
2012-10-25 12:52:06 -0400

    MAde some changes but kept examples as references. Will have to learn how to use floats and ints together. to convert properly. Will work on for next week

--- Java_1/TestingAgain/gen/com/lpuente/testingagain/R.java
+++ Java_1/TestingAgain/gen/com/lpuente/testingagain/R.java
@@ -19,8 +19,10 @@ public final class R {
     }
     public static final class integer {
         public static final int dime=0x7f030001;
+        public static final int kilos=0x7f030005;
         public static final int nickel=0x7f030002;
         public static final int penny=0x7f030003;
+        public static final int pound=0x7f030004;
         public static final int quarter=0x7f030000;
     }
     public static final class menu {
@@ -30,10 +32,12 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int dime=0x7f040006;
         public static final int hello_world=0x7f040001;
+        public static final int kilos=0x7f04000a;
         public static final int menu_settings=0x7f040002;
         public static final int moneyamts=0x7f040004;
         public static final int nickel=0x7f040007;
         public static final int penny=0x7f040008;
+        public static final int pounds=0x7f040009;
         public static final int quarter=0x7f040005;
         public static final int title_activity_main=0x7f040003;
     }
