a80c470c5caf6437b206dc6e69fa8c78a269c27e
Luis Puente <kenshiryu7@fullsail.edu>
2012-11-15 01:58:24 -0500

    making some minor changes to appearance. Taking longer to search up stuff to get this to work.

--- Java_1/Java1_w3/gen/com/lpuente/java1_w3/R.java
+++ Java_1/Java1_w3/gen/com/lpuente/java1_w3/R.java
@@ -15,7 +15,8 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f070001;
+        public static final int textView1=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -30,6 +31,9 @@ public final class R {
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f050003;
+        public static final int MyRB=0x7f050001;
+        public static final int colorET=0x7f050002;
+        public static final int mytheme=0x7f050000;
     }
 }
