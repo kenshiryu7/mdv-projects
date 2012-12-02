d1ea4d7a79885e2912a094f5e2e2d3152d5c7f10
Luis Puente <kenshiryu7@fullsail.edu>
2012-12-02 03:27:42 -0500

    just creating a basic new start. Going to try to implement code from last 2 weeks now

--- Java_2/Java2_w3/gen/com/lpuente/java2_w3/R.java
+++ Java_2/Java2_w3/gen/com/lpuente/java2_w3/R.java
@@ -8,6 +8,9 @@
 package com.lpuente.java2_w3;
 
 public final class R {
+    public static final class array {
+        public static final int contacts=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -15,24 +18,31 @@ public final class R {
         public static final int ic_action_search=0x7f020001;
         public static final int ic_launcher=0x7f020002;
         public static final int iron_man=0x7f020003;
-        public static final int spider_man=0x7f020004;
+        public static final int main_bg=0x7f020004;
+        public static final int row_bg=0x7f020005;
+        public static final int spider_man=0x7f020006;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button1=0x7f080000;
+        public static final int list_image=0x7f080001;
+        public static final int list_text=0x7f080002;
+        public static final int menu_settings=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int list_item=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int add_button=0x7f050004;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
