5cf6a2784d2895aa9cccf8187420dfcc28dc8891
Luis Puente <kenshiryu7@fullsail.edu>
2012-12-06 05:12:18 -0500

    ok after doing the app It didnt work. I copied and pasted it into another file. Replaced it back into this file. and it works. What in the world? Last time eclipse went crazy on me and i had to reboot it and the laptop. i wonder if its doing something like that again

--- TryNew/gen/com/example/quickapp/R.java
+++ TryNew/gen/com/example/quickapp/R.java
@@ -11,14 +11,20 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int blank=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int iv_photo=0x7f070002;
+        public static final int lst_contacts=0x7f070000;
+        public static final int menu_settings=0x7f070004;
+        public static final int tv_details=0x7f070003;
+        public static final int tv_name=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int lv_layout=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
@@ -27,6 +33,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int none=0x7f040004;
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
