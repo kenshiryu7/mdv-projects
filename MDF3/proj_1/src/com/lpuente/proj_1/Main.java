a5f1eefbb917b46193b4806d5b425f341d9acf08
Luis Puente <kenshiryu7@fullsail.edu>
2013-02-07 20:46:29 -0500

    adding no title feature as screen management. Hopefully it counts. Finishing i suppose

--- MDF3/proj_1/src/com/lpuente/proj_1/Main.java
+++ MDF3/proj_1/src/com/lpuente/proj_1/Main.java
@@ -7,13 +7,22 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
+
 
 public class Main extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        
+        //trying simple window management feature
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.main_lay);
         
         //get intent method
