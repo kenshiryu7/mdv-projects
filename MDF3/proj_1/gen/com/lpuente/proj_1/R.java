916424642518162212aefa489f66b9d7180f71f1
Luis Puente <kenshiryu7@fullsail.edu>
2013-02-07 19:58:08 -0500

    got the manifest to work. Stack overflow helped. URI and URL isnt super hard. Just some fine tuning to get used to it.

--- MDF3/proj_1/gen/com/lpuente/proj_1/R.java
+++ MDF3/proj_1/gen/com/lpuente/proj_1/R.java
@@ -15,10 +15,7 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int WebView01=0x7f070001;
-        public static final int WebView02=0x7f070002;
-        public static final int WebView03=0x7f070003;
-        public static final int menu_settings=0x7f070004;
+        public static final int menu_settings=0x7f070001;
         public static final int webView1=0x7f070000;
     }
     public static final class layout {
