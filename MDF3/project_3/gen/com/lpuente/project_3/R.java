5537b19f646904664c566abf429c2f3e42575012
Luis Puente <kenshiryu7@fullsail.edu>
2013-02-20 14:53:36 -0500

    starting the widget. Following videos. Will make adjustments later.

--- MDF3/project_3/gen/com/lpuente/project_3/R.java
+++ MDF3/project_3/gen/com/lpuente/project_3/R.java
@@ -11,49 +11,57 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int headphone=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int accellText=0x7f08000c;
-        public static final int locationB=0x7f080003;
-        public static final int menu_settings=0x7f08000d;
-        public static final int notifyB=0x7f080002;
-        public static final int playB=0x7f080006;
-        public static final int playVid=0x7f080008;
-        public static final int tab1=0x7f080004;
-        public static final int tab2=0x7f080001;
-        public static final int tab3=0x7f08000b;
-        public static final int tabhost=0x7f080000;
-        public static final int textView1=0x7f080005;
-        public static final int textView2=0x7f080007;
-        public static final int textView3=0x7f080009;
-        public static final int vidview=0x7f08000a;
+        public static final int accellText=0x7f09000c;
+        public static final int locationB=0x7f090003;
+        public static final int menu_settings=0x7f09000f;
+        public static final int notifyB=0x7f090002;
+        public static final int playB=0x7f090006;
+        public static final int playVid=0x7f090008;
+        public static final int tab1=0x7f090004;
+        public static final int tab2=0x7f090001;
+        public static final int tab3=0x7f09000b;
+        public static final int tabhost=0x7f090000;
+        public static final int textView1=0x7f090005;
+        public static final int textView2=0x7f090007;
+        public static final int textView3=0x7f090009;
+        public static final int vidview=0x7f09000a;
+        public static final int widget_butt=0x7f09000e;
+        public static final int widget_tv=0x7f09000d;
     }
     public static final class layout {
         public static final int main_lay=0x7f030000;
+        public static final int widget_layout=0x7f030001;
     }
     public static final class menu {
-        public static final int main_lay=0x7f070000;
+        public static final int main_lay=0x7f080000;
     }
     public static final class raw {
-        public static final int doing_time=0x7f040000;
-        public static final int mxpx=0x7f040001;
+        public static final int doing_time=0x7f050000;
+        public static final int mxpx=0x7f050001;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int listen=0x7f050006;
-        public static final int main_text=0x7f050004;
-        public static final int menu_settings=0x7f050002;
-        public static final int notify=0x7f05000a;
-        public static final int showLocText=0x7f050009;
-        public static final int stop=0x7f050005;
-        public static final int title_activity_main=0x7f050003;
-        public static final int video=0x7f050008;
-        public static final int watch=0x7f050007;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int listen=0x7f060006;
+        public static final int main_text=0x7f060004;
+        public static final int menu_settings=0x7f060002;
+        public static final int notify=0x7f06000a;
+        public static final int showLocText=0x7f060009;
+        public static final int stop=0x7f060005;
+        public static final int title_activity_main=0x7f060003;
+        public static final int video=0x7f060008;
+        public static final int watch=0x7f060007;
+        public static final int widget_text=0x7f06000b;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
+    }
+    public static final class xml {
+        public static final int my_widget=0x7f040000;
     }
 }
