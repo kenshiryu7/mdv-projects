5165fc55dafe783062964b8c5e82f4c9f616447d
Luis Puente <kenshiryu7@fullsail.edu>
2013-02-21 01:57:04 -0500

    tried tabactivity. ITS DEPRECATED!!!! whatever basically now going the cheesy get some requirements in route. Going to try widgets again

--- MDF3/WIDGETS/src/com/lpuente/project_3/Main.java
+++ MDF3/WIDGETS/src/com/lpuente/project_3/Main.java
@@ -1,25 +1,42 @@
 package com.lpuente.project_3;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class Main extends Activity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+
+
+
+public class Main extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_lay);
+		
+		
+		
+		Button toMain = (Button) findViewById(R.id.main_butt);
+		
+		toMain.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Intent mainInt = new Intent(Main.this, TheMainStuff.class);
+				
+				startActivity(mainInt);
+				
+			}
+		});
 	}
 
-	
-	
-	//one of these days I'll use the menu
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
 }
