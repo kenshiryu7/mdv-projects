9097bac8b5f4eb6502bf8b2d532d146969f1c798
Luis Puente <kenshiryu7@fullsail.edu>
2013-04-18 08:50:17 -0400

    added additional images and back button on title bar of the full image screen to go back to the grid.

--- APD2/Canine_Care/src/com/lpuente/canine_care/FullImage.java
+++ APD2/Canine_Care/src/com/lpuente/canine_care/FullImage.java
@@ -3,6 +3,8 @@ package com.lpuente.canine_care;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class FullImage extends Activity {
@@ -11,6 +13,10 @@ public class FullImage extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.full_image);
+        
+     // Show the Up button in the action bar.
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+	        
  
         // get intent data
         Intent i = getIntent();
@@ -24,5 +30,22 @@ public class FullImage extends Activity {
         ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
         imageView.setImageResource(imageAdapter.mThumbIds[position]);
     }
+    
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			
+//Back to....
+			NavUtils.navigateUpTo(this,
+					new Intent(this, ImageGrid.class));
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
  
 }
--- APD2/Canine_Care/src/com/lpuente/canine_care/ImageAdapter.java
+++ APD2/Canine_Care/src/com/lpuente/canine_care/ImageAdapter.java
@@ -15,7 +15,32 @@ public class ImageAdapter extends BaseAdapter {
     //Prefer to have seperate arrays for only the proper images selected. Chocolate leads to chocolate IMGs for
     //example rather than opening ALL images. Will see if I can figure this out in TIME.
    
-    public Integer[] mThumbIds = {R.drawable.choco, R.drawable.coco2, R.drawable.coco3, R.drawable.coco4};
+    public Integer[] mThumbIds = {
+    		R.drawable.choco, R.drawable.coco2, R.drawable.coco3, R.drawable.coco4,
+    		
+    		R.drawable.onions, R.drawable.onion2, R.drawable.onion3, R.drawable.onion4,
+    		
+    		R.drawable.grapes, R.drawable.grape2, R.drawable.grape3, R.drawable.grape4,
+    		
+    		R.drawable.coffee, R.drawable.coffee2, R.drawable.coffee3, R.drawable.coffee4,
+    		
+    		R.drawable.mnuts, R.drawable.mnuts2, R.drawable.mnuts3, R.drawable.mnuts4,
+    		
+    		R.drawable.azalea, R.drawable.azalea2, R.drawable.azalea3, R.drawable.azalea4,
+    		
+    		R.drawable.crocus, R.drawable.crocus2, R.drawable.crocus3, R.drawable.crocus4,
+    		
+    		R.drawable.foxglove, R.drawable.fox2, R.drawable.fox3, R.drawable.fox4,
+    		
+    		R.drawable.juniper, R.drawable.junip2, R.drawable.junip3, R.drawable.junip4,
+    		
+    		R.drawable.mistletoe, R.drawable.mtoe2, R.drawable.mtoe3, R.drawable.mtoe4,
+    		
+    		R.drawable.afreeze, R.drawable.eth2, R.drawable.eth3, R.drawable.eth4,
+    		
+    		R.drawable.ratpoison, R.drawable.rat2, R.drawable.rat3, R.drawable.rat4,
+    		
+    		R.drawable.snailbait, R.drawable.snail2, R.drawable.snail3, R.drawable.snail4};
     
  // Constructor
     public ImageAdapter(Context c){
